c76604b946737e3ddf08036bb746bf7fa0d38ab8
Mohammad Mehdi Esmaeili <43288493+witahair@users.noreply.github.com>
2023-02-08 01:05:41 +0330

    Add files via upload

---
+++ army_status.py
@@ -0,0 +1,8 @@
+#the program is printing thr army status with conditions
+age=float (input ("Enter your age:"))
+gender=input("Enter your gender:")
+if age >= 18 and gender == "male":
+    print("you go army")
+else:
+   print("stay home")
+   
